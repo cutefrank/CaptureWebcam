428ef649f576472b57b001ed9540ee516fc997e4
cutefrank <littlescore@gmail.com>
2015-09-24 19:46:14 +0800

    Get JPEG files by WebCam
    it's easy to use.

---
+++ CaptureWebcam/CaptureWebcam/CaptureWebcam.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class CaptureWebcam
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.saveBtn = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // saveBtn
+            // 
+            this.saveBtn.Location = new System.Drawing.Point(-1, -1);
+            this.saveBtn.Name = "saveBtn";
+            this.saveBtn.Size = new System.Drawing.Size(84, 31);
+            this.saveBtn.TabIndex = 0;
+            this.saveBtn.Text = "Save";
+            this.saveBtn.UseVisualStyleBackColor = true;
+            this.saveBtn.Click += new System.EventHandler(this.saveBtn_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(-1, 29);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(725, 493);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // CaptureWebcam
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(725, 524);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.saveBtn);
+            this.Name = "CaptureWebcam";
+            this.Text = "CaptureWebcam";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button saveBtn;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ CaptureWebcam/CaptureWebcam/CaptureWebcam.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+//加入參考
+using Emgu.CV;
+using Emgu.CV.Structure;
+using System.Drawing.Imaging;
+using System.IO;
+
+
+namespace WindowsFormsApplication1
+{
+    public partial class CaptureWebcam : Form
+    {
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+            timer1.Interval = 10000; //每十秒存一次圖檔
+            timer1.Tick += timer1_Tick;
+        }
+
+        void timer1_Tick(object sender, EventArgs e)
+        {
+            //存圖檔
+            FileInfo fp = new FileInfo(@"C:\CaptureFiles\" + DateTime.Now.ToString("MMddyyHmmss") + @".jpg");
+            FileStream fs = fp.Create();
+            pictureBox1.Image.Save(fs, System.Drawing.Imaging.ImageFormat.Jpeg);
+            fs.Close();
+        }
+
+        private Capture cap = null;// Webcam物件
+
+        public CaptureWebcam()
+        {
+            InitializeComponent();
+            cap = new Capture(0); // 連結到攝影機0，如果你有兩台攝影機，第二台就是1
+            Application.Idle += new EventHandler(Application_Idle); // 在Idle的event下，把畫面設定到pictureBox上(當然你也可以用timer事件)
+        }
+
+        void Application_Idle(object sender, EventArgs e)
+        {
+            Image<Bgr, Byte>frame = cap.QueryFrame(); // 去query該畫面
+            pictureBox1.Image = frame.ToBitmap(); // 把畫面轉換成bitmap型態，在餵給pictureBox元件
+        }
+
+        
+        //手動保存圖片
+        private void saveBtn_Click(object sender, System.EventArgs e)
+        {
+            bool isSave = true;
+            SaveFileDialog saveImageDialog = new SaveFileDialog();
+            saveImageDialog.Title = "圖片保存";
+            saveImageDialog.Filter = @"jpeg|*.jpg|bmp|*.bmp|gif|*.gif";
+
+            if (saveImageDialog.ShowDialog() == DialogResult.OK)
+            {
+                string fileName = saveImageDialog.FileName.ToString();
+
+                if (fileName != "" && fileName != null)
+                {
+                    string fileExtName = fileName.Substring(fileName.LastIndexOf(".") + 1).ToString();
+
+                    System.Drawing.Imaging.ImageFormat imgformat = null;
+
+                    if (fileExtName != "")
+                    {
+                        switch (fileExtName)
+                        {
+                            case "jpg":
+                                imgformat = System.Drawing.Imaging.ImageFormat.Jpeg;
+                                break;
+                            case "bmp":
+                                imgformat = System.Drawing.Imaging.ImageFormat.Bmp;
+                                break;
+                            case "gif":
+                                imgformat = System.Drawing.Imaging.ImageFormat.Gif;
+                                break;
+                            default:
+                                MessageBox.Show("只能存取為: jpg,bmp,gif 格式");
+                                isSave = false;
+                                break;
+                        }
+
+                    }
+
+                    //預設保存為JPG格式
+                    if (imgformat == null)
+                    {
+                        imgformat = System.Drawing.Imaging.ImageFormat.Jpeg;
+                    }
+
+                    if (isSave)
+                    {
+                        try
+                        {
+                            this.pictureBox1.Image.Save(fileName, imgformat);
+                            //MessageBox.Show("圖片已經成功保存!");
+                        }
+                        catch
+                        {
+                            MessageBox.Show("保存失敗,你還沒有截取過圖片或已經清空圖片!");
+                        }
+                    }
+
+                }
+
+            }
+        }
+
+
+    }
+}
